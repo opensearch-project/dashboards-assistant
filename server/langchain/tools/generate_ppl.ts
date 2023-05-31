203e5bae4102e5799168a16f193583b41c1a3da6
Joshua Li <joshuali925@gmail.com>
2023-05-31 22:09:32 +0000

    add tracing and adjust UI
    Signed-off-by: Joshua Li <joshuali925@gmail.com>

--- server/langchain/tools/generate_ppl.ts
+++ server/langchain/tools/generate_ppl.ts
@@ -4,8 +4,11 @@
  */
 
 import { promises as fs } from 'fs';
+import process from 'process';
 import { request as requestEntities } from '../chains/entities_finder';
 import { request as requestPPLGenerator } from '../chains/ppl_generator';
+
+process.env.LANGCHAIN_TRACING = 'true';
 
 interface GeneratePPLOptions {
   question: string;
