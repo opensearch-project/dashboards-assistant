b363db65040f984d2939d1f557cba39e2c54c3f2
Joshua Li <joshuali925@gmail.com>
2023-05-31 21:20:26 +0000

    temporarily add workarounds for event analytics and logging
    Signed-off-by: Joshua Li <joshuali925@gmail.com>

--- server/langchain/tools/generate_ppl.ts
+++ server/langchain/tools/generate_ppl.ts
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import { promises as fs } from 'fs';
 import { request as requestEntities } from '../chains/entities_finder';
 import { request as requestPPLGenerator } from '../chains/ppl_generator';
 
@@ -13,11 +14,25 @@ interface GeneratePPLOptions {
   fields: string;
 }
 export const generatePPL = async (options: GeneratePPLOptions) => {
-  const entitiesHints = await requestEntities(options.question, options.fields);
-  // const input = `${options.question} ${entitiesHints}, time field is \`${options.timeField}\`, index is \`${options.index}\``;
-  const input = `${options.question} ${entitiesHints}, index is \`${options.index}\``;
-  console.info('❗input:', input);
-  const ppl = await requestPPLGenerator(input);
-  console.info('❗ppl:', ppl);
-  return ppl;
+  try {
+    const entitiesHints = await requestEntities(options.question, options.fields);
+    const input = `${options.question}\n${entitiesHints}, index is \`${options.index}\``;
+    const ppl = await requestPPLGenerator(input);
+    logToFile({ question: options.question, input, ppl });
+    ppl.query = ppl.query.replace(/^source\s*=\s*`(.+?)`/, 'source=$1'); // workaround for https://github.com/opensearch-project/dashboards-observability/issues/509
+    return ppl;
+  } catch (error) {
+    logToFile({ question: options.question, error });
+  }
+};
+
+const logToFile = async (status: object) => {
+  await fs.mkdir(`${__dirname}/../../../.logs`, { recursive: true });
+  fs.appendFile(
+    `${__dirname}/../../../.logs/ppl_generator.log`,
+    JSON.stringify({
+      timestamp: new Date().toISOString(),
+      ...status,
+    }) + '\n'
+  );
 };
