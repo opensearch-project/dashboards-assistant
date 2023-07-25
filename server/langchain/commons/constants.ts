31f2ec30c60eb91ab4619129d8ae4afea62bee2f
Shenoy Pratik <sgguruda@amazon.com>
2023-07-25 13:45:05 -0700

    update ml commons temperature
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>

--- server/langchain/commons/constants.ts
+++ server/langchain/commons/constants.ts
@@ -7,7 +7,7 @@ export const ML_COMMONS_BASE_API = '/_plugins/_ml/models';
 
 // Below params are inspired from langchain defaults
 export const ANTHROPIC_DEFAULT_PARAMS = {
-  temperature: 1,
+  temperature: 0.0000001,
   max_tokens_to_sample: 2048,
 };
 
