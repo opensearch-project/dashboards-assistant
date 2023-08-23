cbdd81601f62fdc5b377945425b042217d8074c8
Daniel Dong <danieldong51@amazon.com>
2023-08-23 11:54:01 -0700

    Updated the description for services tool
    Signed-off-by: Daniel Dong <danieldong51@amazon.com>

--- server/langchain/tools/tool_sets/traces.ts
+++ server/langchain/tools/tool_sets/traces.ts
@@ -37,7 +37,8 @@ export class TracesTools extends PluginToolsFactory {
     }),
     new DynamicTool({
       name: TracesTools.TOOL_NAMES.SERVICES,
-      description: 'Use this to get information about each service in trace analytics.',
+      description:
+        'Use this to get information about each service in trace analytics. The tool response includes the key, doc_count, error_count.doc_count, average_latency_nanos.value, average_latency.value, and error_rate.value. The key is the name of the service. The doc_count is the number of spans in the service. The error_count.doc_count is the number of traces with errors in the service. The average_latency.value is the average latency in milliseconds. The error_rate.value is the percentage of traces that had an error.',
       func: swallowErrors(async () => this.getServices()),
     }),
   ];
