b946d8e9d05d69edd559a2709474f23575700285
SuZhou-Joe <suzhou@amazon.com>
2023-11-10 16:13:09 +0800

    feat: make windows flow pass
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>

---
+++ babel.config.js
@@ -0,0 +1,26 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// babelrc doesn't respect NODE_PATH anymore but using require does.
+// Alternative to install them locally in node_modules
+module.exports = function (api) {
+  // ensure env is test so that this config won't impact build or dev server
+  if (api.env('test')) {
+    return {
+      presets: [
+        require('@babel/preset-env'),
+        require('@babel/preset-react'),
+        require('@babel/preset-typescript'),
+      ],
+      plugins: [
+        [require('@babel/plugin-transform-runtime'), { regenerator: true }],
+        require('@babel/plugin-proposal-class-properties'),
+        require('@babel/plugin-proposal-object-rest-spread'),
+        [require('@babel/plugin-transform-modules-commonjs'), { allowTopLevelThis: true }],
+      ],
+    };
+  }
+  return {};
+};
