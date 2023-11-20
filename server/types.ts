b20f382535aead50e95775ec911c7ab2ae3209de
SuZhou-Joe <suzhou@amazon.com>
2023-11-20 14:15:50 +0800

    feat: add mechannism to register messageParser
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>

--- server/types.ts
+++ server/types.ts
@@ -3,12 +3,45 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import { IMessage } from '../common/types/chat_saved_object_attributes';
 import { ILegacyClusterClient, Logger } from '../../../src/core/server';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface AssistantPluginSetup {}
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface AssistantPluginStart {}
+
+export interface IMessageParserHelper {
+  addMessage: (message: IMessage) => boolean;
+}
+
+export interface Interaction {
+  input: string;
+  response: string;
+}
+
+export interface MessageParser {
+  /**
+   * The id of the parser, should be unique among the parsers.
+   */
+  id: string;
+  /**
+   * Order declare the order message parser will be execute.
+   * parser with order 2 will be execute before parser with order 1.
+   */
+  order?: number;
+  /**
+   * parserProvider is the callback that will be triggered in each message
+   */
+  parserProvider: (
+    interaction: Interaction,
+    messageParserHelper: IMessageParserHelper
+  ) => Promise<unknown>;
+}
+
+export interface RoutesOptions {
+  messageParsers: MessageParser[];
+}
 
 declare module '../../../src/core/server' {
   interface RequestHandlerContext {
