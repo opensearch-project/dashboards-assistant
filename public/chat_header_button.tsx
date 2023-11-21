b9ee06a2fb39e2a2ee3d04ca9884ab52443dc16e
Yulong Ruan <ruanyl@amazon.com>
2023-11-21 17:55:46 +0800

    User starts chat from header input (#33)
    * add input box in header to allow user to trigger chatbot
    
    ---------
    
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>

--- public/chat_header_button.tsx
+++ public/chat_header_button.tsx
@@ -3,9 +3,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { EuiHeaderSectionItemButton, EuiIcon } from '@elastic/eui';
+import { EuiBadge, EuiFieldText, EuiIcon } from '@elastic/eui';
 import classNames from 'classnames';
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useCallback, useMemo, useRef, useState, useEffect } from 'react';
 import { useEffectOnce } from 'react-use';
 import { ApplicationStart } from '../../../src/core/public';
 import { ChatFlyout } from './chat_flyout';
@@ -37,7 +37,10 @@ export const HeaderChatButton: React.FC<HeaderChatButtonProps> = (props) => {
   const [preSelectedTabId, setPreSelectedTabId] = useState<TabId | undefined>(undefined);
   const [traceId, setTraceId] = useState<string | undefined>(undefined);
   const [chatSize, setChatSize] = useState<number | 'fullscreen' | 'dock-right'>('dock-right');
+  const [query, setQuery] = useState('');
+  const [inputFocus, setInputFocus] = useState(false);
   const flyoutFullScreen = chatSize === 'fullscreen';
+  const inputRef = useRef<HTMLInputElement>(null);
 
   if (!flyoutLoaded && flyoutVisible) flyoutLoaded = true;
 
@@ -92,14 +95,77 @@ export const HeaderChatButton: React.FC<HeaderChatButtonProps> = (props) => {
     ]
   );
 
+  const onKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter' && query.trim().length > 0) {
+      // open chat window
+      setFlyoutVisible(true);
+      // start a new chat
+      props.assistantActions.loadChat();
+      // send message
+      props.assistantActions.send({
+        type: 'input',
+        contentType: 'text',
+        content: query,
+        context: { appId },
+      });
+      // reset query to empty
+      setQuery('');
+      if (inputRef.current) {
+        inputRef.current.blur();
+      }
+    }
+  };
+
+  const onKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Escape') {
+      inputRef.current?.blur();
+    }
+  };
+
+  useEffect(() => {
+    const onGlobalMouseUp = (e: KeyboardEvent) => {
+      if (e.metaKey && e.key === '/') {
+        inputRef.current?.focus();
+      }
+    };
+    document.addEventListener('keydown', onGlobalMouseUp);
+
+    return () => {
+      document.removeEventListener('keydown', onGlobalMouseUp);
+    };
+  }, []);
+
   return (
     <>
-      <EuiHeaderSectionItemButton
-        className={classNames('llm-chat-header-icon-wrapper')}
-        onClick={() => setFlyoutVisible(!flyoutVisible)}
-      >
-        <EuiIcon type={chatIcon} size="l" />
-      </EuiHeaderSectionItemButton>
+      <div className={classNames('llm-chat-header-icon-wrapper')}>
+        <EuiFieldText
+          inputRef={inputRef}
+          compressed
+          value={query}
+          onFocus={() => setInputFocus(true)}
+          onBlur={() => setInputFocus(false)}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Ask question"
+          onKeyPress={onKeyPress}
+          onKeyUp={onKeyUp}
+          prepend={
+            <EuiIcon type={chatIcon} size="l" onClick={() => setFlyoutVisible(!flyoutVisible)} />
+          }
+          append={
+            <span className="llm-chat-header-shortcut">
+              {inputFocus ? (
+                <EuiBadge className="llm-chat-header-shortcut-enter" color="hollow">
+                  ⏎
+                </EuiBadge>
+              ) : (
+                <EuiBadge className="llm-chat-header-shortcut-cmd" color="hollow">
+                  ⌘ + /
+                </EuiBadge>
+              )}
+            </span>
+          }
+        />
+      </div>
       <ChatContext.Provider value={chatContextValue}>
         <ChatStateProvider>
           <SetContext assistantActions={props.assistantActions} />
