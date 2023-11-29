39155e794b3a9e92491014224ba62a7d807739c2
Lin Wang <wonglam@amazon.com>
2023-11-29 16:30:59 +0800

    Feat add experiment icon and tooltip langchain (#21)
    * feat: add form experiment badge
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    * refactor: replace filter length with every
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    * refactor: remove unnecessary useCallback
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    * refactor: renameModelOpen to renameModalOpen
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    * feat: update link and margin
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    ---------
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>

---
+++ public/components/chat_experimental_badge.tsx
@@ -0,0 +1,53 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import React, { useState } from 'react';
+import {
+  EuiPopover,
+  EuiButtonIcon,
+  EuiTitle,
+  EuiHorizontalRule,
+  EuiText,
+  EuiLink,
+} from '@elastic/eui';
+
+interface ChatExperimentalBadgeProps {
+  onClick?: React.MouseEventHandler<HTMLDivElement>;
+}
+
+export const ChatExperimentalBadge = ({ onClick }: ChatExperimentalBadgeProps) => {
+  const [visible, setVisible] = useState(false);
+
+  const closePopover = () => {
+    setVisible(false);
+  };
+
+  const handleIconClick = () => {
+    setVisible((flag) => !flag);
+  };
+
+  return (
+    <EuiPopover
+      isOpen={visible}
+      button={<EuiButtonIcon color="text" iconType="beaker" onClick={handleIconClick} />}
+      closePopover={closePopover}
+      onClick={onClick}
+    >
+      <EuiTitle size="xs">
+        <h4>Experimental</h4>
+      </EuiTitle>
+      <EuiHorizontalRule margin="s" />
+      <EuiText>
+        This is an experimental feature.
+        <br />
+        Send feedback via{' '}
+        <EuiLink href="https://forum.opensearch.org/t/feedback-opensearch-assistant/16741">
+          Forum
+        </EuiLink>{' '}
+        or <EuiLink href="https://opensearch.slack.com/channels/assistant-feedback">Slack</EuiLink>.
+      </EuiText>
+    </EuiPopover>
+  );
+};
---
+++ public/components/chat_window_header_title.tsx
@@ -0,0 +1,127 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {
+  EuiContextMenuItem,
+  EuiContextMenuPanel,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiPopover,
+  EuiButtonIcon,
+} from '@elastic/eui';
+import React, { useCallback, useState } from 'react';
+import { useChatContext } from '../contexts/chat_context';
+import { useChatActions } from '../hooks/use_chat_actions';
+import { NotebookNameModal } from './notebook/notebook_name_modal';
+import { ChatExperimentalBadge } from './chat_experimental_badge';
+import { useCore } from '../contexts/core_context';
+import { useChatState } from '../hooks/use_chat_state';
+import { useSaveChat } from '../hooks/use_save_chat';
+import { EditConversationNameModal } from './edit_conversation_name_modal';
+
+export const ChatWindowHeaderTitle = React.memo(() => {
+  const chatContext = useChatContext();
+  const { loadChat } = useChatActions();
+  const core = useCore();
+  const [isPopoverOpen, setPopoverOpen] = useState(false);
+  const [isRenameModalOpen, setRenameModalOpen] = useState(false);
+  const { chatState } = useChatState();
+  const { saveChat } = useSaveChat();
+
+  const onButtonClick = useCallback(() => {
+    setPopoverOpen((flag) => !flag);
+  }, []);
+
+  const closePopover = useCallback(() => {
+    setPopoverOpen(false);
+  }, []);
+
+  const handleEditConversationClose = useCallback(
+    (status: 'updated' | string, newTitle?: string) => {
+      if (status === 'updated') {
+        chatContext.setTitle(newTitle);
+      }
+      setRenameModalOpen(false);
+    },
+    [chatContext]
+  );
+
+  const button = (
+    <EuiFlexGroup
+      style={{ maxWidth: '300px', padding: '0 8px' }}
+      gutterSize="xs"
+      alignItems="center"
+    >
+      <EuiFlexItem onClick={onButtonClick} style={{ overflow: 'hidden' }}>
+        <h3 className="eui-textTruncate">
+          {chatContext.sessionId ? chatContext.title : 'OpenSearch Assistant'}
+        </h3>
+      </EuiFlexItem>
+      <EuiFlexItem grow={false}>
+        <ChatExperimentalBadge onClick={closePopover} />
+      </EuiFlexItem>
+      <EuiFlexItem onClick={onButtonClick} grow={false}>
+        <EuiButtonIcon color="text" iconType="arrowDown" />
+      </EuiFlexItem>
+    </EuiFlexGroup>
+  );
+
+  const items = [
+    <EuiContextMenuItem
+      disabled={!chatContext.sessionId}
+      key="rename-conversation"
+      onClick={() => {
+        closePopover();
+        setRenameModalOpen(true);
+      }}
+    >
+      Rename conversation
+    </EuiContextMenuItem>,
+    <EuiContextMenuItem
+      key="new-conversation"
+      onClick={() => {
+        closePopover();
+        loadChat(undefined);
+      }}
+    >
+      New conversation
+    </EuiContextMenuItem>,
+    <EuiContextMenuItem
+      key="save-as-notebook"
+      onClick={() => {
+        const modal = core.overlays.openModal(
+          <NotebookNameModal onClose={() => modal.close()} saveChat={saveChat} />
+        );
+        closePopover();
+      }}
+      // User only can save conversation when he send a message at least.
+      disabled={chatState.messages.every((item) => item.type !== 'input')}
+    >
+      Save to notebook
+    </EuiContextMenuItem>,
+  ];
+
+  return (
+    <>
+      <EuiPopover
+        id="conversationTitle"
+        button={button}
+        isOpen={isPopoverOpen}
+        closePopover={closePopover}
+        panelPaddingSize="none"
+        anchorPosition="downRight"
+      >
+        <EuiContextMenuPanel size="m" items={items} />
+      </EuiPopover>
+      {isRenameModalOpen && (
+        <EditConversationNameModal
+          sessionId={chatContext.sessionId!}
+          onClose={handleEditConversationClose}
+          defaultTitle={chatContext.title!}
+        />
+      )}
+    </>
+  );
+});
