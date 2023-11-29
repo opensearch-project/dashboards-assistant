f331a8d543b1ad314c5a078bc14245059fc76927
tygao <tygao@amazon.com>
2023-11-29 17:29:44 +0800

    fix lint error
    Signed-off-by: tygao <tygao@amazon.com>

--- server/routes/langchain_routes.ts
+++ server/routes/langchain_routes.ts
@@ -130,48 +130,4 @@ export function registerLangchainRoutes(router: IRouter) {
       }
     }
   );
-
-  router.post(
-    {
-      path: ASSISTANT_API.FEEDBACK,
-      validate: {
-        body: schema.object({
-          metadata: schema.object({
-            user: schema.string(),
-            tenant: schema.string(),
-            type: schema.string(),
-            sessionId: schema.maybe(schema.string()),
-            traceId: schema.maybe(schema.string()),
-            error: schema.maybe(schema.boolean()),
-            selectedIndex: schema.maybe(schema.string()),
-          }),
-          input: schema.string(),
-          output: schema.string(),
-          correct: schema.boolean(),
-          expectedOutput: schema.string(),
-          comment: schema.string(),
-        }),
-      },
-    },
-    async (
-      context,
-      request,
-      response
-    ): Promise<IOpenSearchDashboardsResponse<HttpResponsePayload | ResponseError>> => {
-      try {
-        await context.core.opensearch.client.asCurrentUser.index({
-          index: LLM_INDEX.FEEDBACK,
-          body: { ...request.body, timestamp: new Date().toISOString() },
-        });
-
-        return response.ok();
-      } catch (error) {
-        console.error(error);
-        return response.custom({
-          statusCode: error.statusCode || 500,
-          body: error.message,
-        });
-      }
-    }
-  );
 }
