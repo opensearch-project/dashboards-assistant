cab050f86a89dac515e9bcc25b23ae1164022ae3
Yulong Ruan <ruanyl@amazon.com>
2023-11-29 16:44:47 +0800

    remove terms and conditions from chat window (#20)
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>

--- public/components/terms_and_conditions.tsx
+++ public/components/terms_and_conditions.tsx
@@ -4,7 +4,7 @@
  */
 
 import React from 'react';
-import { EuiEmptyPrompt, EuiLink, EuiText } from '@elastic/eui';
+import { EuiEmptyPrompt, EuiText } from '@elastic/eui';
 
 interface Props {
   username: string;
@@ -13,13 +13,12 @@ interface Props {
 export const TermsAndConditions = (props: Props) => {
   return (
     <EuiEmptyPrompt
-      style={{ padding: 0 }}
       iconType="cheer"
       iconColor="primary"
       titleSize="s"
       body={
         <EuiText color="default">
-          <p>Welcome {props.username} to the OpenSearch Assistant</p>
+          <p>Welcome {props.username} to the OpenSearch Assistant!</p>
           <p>I can help you analyze data, create visualizations, and get other insights.</p>
           <p>How can I help?</p>
           <EuiText size="xs" color="subdued">
@@ -28,13 +27,6 @@ export const TermsAndConditions = (props: Props) => {
           </EuiText>
         </EuiText>
       }
-      actions={[
-        <EuiText size="xs">
-          <EuiLink target="_blank" href="/">
-            Terms & Conditions
-          </EuiLink>
-        </EuiText>,
-      ].filter(Boolean)}
     />
   );
 };
