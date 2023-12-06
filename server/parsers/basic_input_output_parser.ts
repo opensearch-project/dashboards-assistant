9f8817cbfcde138405230a6a4d3894c98b034a9b
SuZhou-Joe <suzhou@amazon.com>
2023-12-06 12:09:58 +0800

    feat: integrate suggestions (#44)
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>

--- server/parsers/basic_input_output_parser.ts
+++ server/parsers/basic_input_output_parser.ts
@@ -17,6 +17,15 @@ export const BasicInputOutputParser = {
   order: 0,
   id: 'output_message',
   async parserProvider(interaction: Interaction) {
+    const suggestedOutputString = interaction.additional_info?.['QuestionSuggestor.output'] as
+      | string
+      | null;
+    let suggestedActions: string[] = [];
+    try {
+      suggestedActions = JSON.parse(suggestedOutputString || '[]');
+    } catch (e) {
+      suggestedActions = [];
+    }
     const inputItem: IInput = {
       type: 'input',
       contentType: 'text',
@@ -28,6 +37,12 @@ export const BasicInputOutputParser = {
         contentType: 'markdown',
         content: sanitize(interaction.response),
         traceId: interaction.interaction_id,
+        suggestedActions: suggestedActions
+          .filter((item) => item)
+          .map((item) => ({
+            actionType: 'send_as_input',
+            message: item,
+          })),
       },
     ];
     return [inputItem, ...outputItems];
