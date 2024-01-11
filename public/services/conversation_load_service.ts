af7ac9d0dd9109b41b57528243342643595a1783
SuZhou-Joe <suzhou@amazon.com>
2024-01-11 15:35:55 +0800

    fix: unit test
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>

---
+++ public/services/conversation_load_service.ts
@@ -0,0 +1,36 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { BehaviorSubject } from 'rxjs';
+import { HttpStart } from '../../../../src/core/public';
+import { IConversation } from '../../common/types/chat_saved_object_attributes';
+import { ASSISTANT_API } from '../../common/constants/llm';
+
+export class ConversationLoadService {
+  status$: BehaviorSubject<
+    'idle' | 'loading' | { status: 'error'; error: Error }
+  > = new BehaviorSubject<'idle' | 'loading' | { status: 'error'; error: Error }>('idle');
+  abortController?: AbortController;
+
+  constructor(private _http: HttpStart) {}
+
+  load = async (conversationId: string) => {
+    this.abortController?.abort();
+    this.status$.next('loading');
+    this.abortController = new AbortController();
+    try {
+      const payload = await this._http.get<IConversation>(
+        `${ASSISTANT_API.CONVERSATION}/${conversationId}`,
+        {
+          signal: this.abortController.signal,
+        }
+      );
+      this.status$.next('idle');
+      return payload;
+    } catch (error) {
+      this.status$.next({ status: 'error', error });
+    }
+  };
+}
