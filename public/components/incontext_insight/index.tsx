05d4986b0b4d97496563bcb48079ce3f50064a9d
Kawika Avilla <kavilla414@gmail.com>
2024-02-08 01:38:07 +0000

    dont give id
    Signed-off-by: Kawika Avilla <kavilla414@gmail.com>

--- public/components/incontext_insight/index.tsx
+++ public/components/incontext_insight/index.tsx
@@ -35,9 +35,8 @@ export interface IncontextInsightProps {
   children: React.ReactNode;
 }
 
-// TODO: add saved objects / config && i18n
+// TODO: add saved objects / config
 const container = document.createElement('div');
-container.id = 'incontext-insight-target';
 document.body.appendChild(container);
 
 export const IncontextInsight = ({ children }: IncontextInsightProps) => {
