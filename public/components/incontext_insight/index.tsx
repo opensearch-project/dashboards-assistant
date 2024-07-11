0a48ddc96d1dfeb2ebc98c910553587a840eb4f6
Miki <miki@amazon.com>
2024-07-10 23:05:47 -0700

    Use EuiSmallButtonIcon
    Signed-off-by: Miki <miki@amazon.com>

--- public/components/incontext_insight/index.tsx
+++ public/components/incontext_insight/index.tsx
@@ -23,7 +23,7 @@ import {
   EuiPanel,
   keys,
   EuiIcon,
-  EuiButtonIcon,
+  EuiSmallButtonIcon,
 } from '@elastic/eui';
 import React, { Children, isValidElement, useEffect, useRef, useState } from 'react';
 import { IncontextInsight as IncontextInsightInput } from '../../types';
@@ -318,7 +318,7 @@ export const IncontextInsight = ({ children, httpSetup }: IncontextInsightProps)
             </EuiFlexItem>
             <EuiFlexItem grow={false}>
               <div>
-                <EuiButtonIcon
+                <EuiSmallButtonIcon
                   title={i18n.translate('assistantDashboards.incontextInsight.closeAssistant', {
                     defaultMessage: 'Close assistant popover',
                   })}
