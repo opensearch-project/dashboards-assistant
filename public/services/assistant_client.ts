aa3e48370d791f2e3a331dbc3ffc45b626d7d819
Yulong Ruan <ruanyl@amazon.com>
2024-09-04 16:17:56 +0800

    tweaks
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>

--- public/services/assistant_client.ts
+++ public/services/assistant_client.ts
@@ -6,15 +6,20 @@
 import { API_BASE } from '../../common/constants/llm';
 import { HttpSetup } from '../../../../src/core/public';
 
+interface Options {
+  dataSourceId?: string;
+}
+
 export class AssistantClient {
   constructor(private http: HttpSetup) {}
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  executeAgent = (agentName: string, parameters: Record<string, any>) => {
+  executeAgent = (agentName: string, parameters: Record<string, any>, options?: Options) => {
     return this.http.fetch({
       method: 'POST',
       path: `${API_BASE}/agents/${agentName}/_execute`,
       body: JSON.stringify(parameters),
+      query: { dataSourceId: options?.dataSourceId },
     });
   };
 }
