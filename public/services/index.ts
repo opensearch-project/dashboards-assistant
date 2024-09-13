6ee6a8db12e338e365c4bb90e9a7573397786342
Yulong Ruan <ruanyl@amazon.com>
2024-09-13 13:09:01 +0800

    feat: expose an API to check if a give agent config name has agent id configured (#307)
    * feat: expose an API to check if a give agent config name has agent id
    configured
    
    + use agentConfigName instead of agentName to avoid confusing
    
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>
    
    * add changelogs
    
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>
    
    ---------
    
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>

--- public/services/index.ts
+++ public/services/index.ts
@@ -4,12 +4,13 @@
  */
 
 import { createGetterSetter } from '../../../../src/plugins/opensearch_dashboards_utils/public';
-import { UiActionsSetup, UiActionsStart } from '../../../../src/plugins/ui_actions/public';
+import { UiActionsStart } from '../../../../src/plugins/ui_actions/public';
 import { ChromeStart, HttpStart, NotificationsStart } from '../../../../src/core/public';
 import { IncontextInsightRegistry } from './incontext_insight';
 import { ConfigSchema } from '../../common/types/config';
 import { IndexPatternsContract } from '../../../../src/plugins/data/public';
 import { ExpressionsStart } from '../../../../src/plugins/expressions/public';
+import { AssistantServiceStart } from './assistant_service';
 
 export * from './incontext_insight';
 export { ConversationLoadService } from './conversation_load_service';
@@ -36,5 +37,9 @@ export const [getIndexPatterns, setIndexPatterns] = createGetterSetter<IndexPatt
 export const [getExpressions, setExpressions] = createGetterSetter<ExpressionsStart>('Expressions');
 
 export const [getHttp, setHttp] = createGetterSetter<HttpStart>('Http');
+
+export const [getAssistantService, setAssistantService] = createGetterSetter<AssistantServiceStart>(
+  'AssistantServiceStart'
+);
 
 export { DataSourceService, DataSourceServiceContract } from './data_source_service';
