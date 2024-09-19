b8bbda15cfd8a53edd1275384651eaeb4ac1ef1c
Sihan He <hesihan@amazon.com>
2024-09-19 18:56:16 +0800

    Remove telementry plugin
    Signed-off-by: Sihan He <hesihan@amazon.com>

--- public/utils/report_metric.ts
+++ public/utils/report_metric.ts
@@ -4,18 +4,16 @@
  */
 
 import { v4 as uuidv4 } from 'uuid';
+import { METRIC_TYPE, UiStatsMetricType } from '@osd/analytics';
 import { UsageCollectionSetup } from '../../../../src/plugins/usage_collection/public';
 
 export const reportMetric = (
   usageCollection?: UsageCollectionSetup,
   metricAppName: string = 'app',
-  metric: string = 'click'
+  metric: string = 'click',
+  metricType: UiStatsMetricType = METRIC_TYPE.CLICK
 ) => {
   if (usageCollection) {
-    usageCollection.reportUiStats(
-      metricAppName,
-      usageCollection.METRIC_TYPE.CLICK,
-      metric + '-' + uuidv4()
-    );
+    usageCollection.reportUiStats(metricAppName, metricType, metric + '-' + uuidv4());
   }
 };
