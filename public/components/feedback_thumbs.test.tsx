519bb92d54a241f2258a49bedb9c80ad5a363368
Yulong Ruan <ruanyl@amazon.com>
2024-09-27 19:12:06 +0800

    T2viz UI metrics (#312)
    * Add metrics for text2viz
    
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>
    
    * remove new line at the end of file
    
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>
    
    * fix imported types
    
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>
    
    * update changelogs
    
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>
    
    * fix UI flashing
    
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>
    
    * cleanup duplicate imports
    
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>
    
    ---------
    
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>

---
+++ public/components/feedback_thumbs.test.tsx
@@ -0,0 +1,79 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import React from 'react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { METRIC_TYPE } from '@osd/analytics';
+
+import { FeedbackThumbs } from './feedback_thumbs';
+
+describe('<FeedbackThumbs />', () => {
+  it('should report thumbs up metric', () => {
+    const usageCollectionMock = {
+      reportUiStats: jest.fn(),
+      METRIC_TYPE,
+    };
+
+    render(<FeedbackThumbs usageCollection={usageCollectionMock} appName="test-app" />);
+    fireEvent.click(screen.getByLabelText('ThumbsUp'));
+    expect(usageCollectionMock.reportUiStats).toHaveBeenCalledWith(
+      'test-app',
+      METRIC_TYPE.CLICK,
+      expect.stringMatching(/thumbs_up.*/)
+    );
+  });
+
+  it('should report thumbs down metric', () => {
+    const usageCollectionMock = {
+      reportUiStats: jest.fn(),
+      METRIC_TYPE,
+    };
+
+    render(<FeedbackThumbs usageCollection={usageCollectionMock} appName="test-app" />);
+    fireEvent.click(screen.getByLabelText('ThumbsDown'));
+    expect(usageCollectionMock.reportUiStats).toHaveBeenCalledWith(
+      'test-app',
+      METRIC_TYPE.CLICK,
+      expect.stringMatching(/thumbs_down.*/)
+    );
+  });
+
+  it('should only report metric only once', () => {
+    const usageCollectionMock = {
+      reportUiStats: jest.fn(),
+      METRIC_TYPE,
+    };
+
+    render(<FeedbackThumbs usageCollection={usageCollectionMock} appName="test-app" />);
+    // click the button two times
+    fireEvent.click(screen.getByLabelText('ThumbsDown'));
+    fireEvent.click(screen.getByLabelText('ThumbsDown'));
+    expect(usageCollectionMock.reportUiStats).toHaveBeenCalledTimes(1);
+  });
+
+  it('should hide thumbs down button after thumbs up been clicked', () => {
+    const usageCollectionMock = {
+      reportUiStats: jest.fn(),
+      METRIC_TYPE,
+    };
+
+    render(<FeedbackThumbs usageCollection={usageCollectionMock} appName="test-app" />);
+
+    fireEvent.click(screen.getByLabelText('ThumbsUp'));
+    expect(screen.queryByLabelText('ThumbsDown')).toBeNull();
+  });
+
+  it('should hide thumbs up button after thumbs down been clicked', () => {
+    const usageCollectionMock = {
+      reportUiStats: jest.fn(),
+      METRIC_TYPE,
+    };
+
+    render(<FeedbackThumbs usageCollection={usageCollectionMock} appName="test-app" />);
+
+    fireEvent.click(screen.getByLabelText('ThumbsDown'));
+    expect(screen.queryByLabelText('ThumbsUp')).toBeNull();
+  });
+});
--- public/types.ts
+++ public/types.ts
@@ -20,8 +20,11 @@ import { AssistantClient } from './services/assistant_client';
 import { UiActionsSetup, UiActionsStart } from '../../../src/plugins/ui_actions/public';
 import { ExpressionsSetup, ExpressionsStart } from '../../../src/plugins/expressions/public';
 import { SavedObjectsStart } from '../../../src/plugins/saved_objects/public';
+import {
+  UsageCollectionStart,
+  UsageCollectionSetup,
+} from '../../../src/plugins/usage_collection/public';
 
-import { UsageCollectionSetup } from '../../../src/plugins/usage_collection/public';
 import { ConfigSchema } from '../common/types/config';
 
 export interface RenderProps {
@@ -49,6 +52,7 @@ export interface AssistantPluginStartDependencies {
   uiActions: UiActionsStart;
   expressions: ExpressionsStart;
   savedObjects: SavedObjectsStart;
+  usageCollection?: UsageCollectionStart;
 }
 
 export interface AssistantPluginSetupDependencies {
