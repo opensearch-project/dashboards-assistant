14623e8b51669b550f619934e66b2deb6302a91b
hutiechuan <hutiech@amazon.com>
2025-02-07 11:04:41 +0800

    Add i18n for error message
    Signed-off-by: hutiechuan <hutiech@amazon.com>

--- server/routes/summary_routes.ts
+++ server/routes/summary_routes.ts
@@ -137,7 +137,9 @@ export function registerSummaryAssistantRoutes(
         const insight = response.body.inference_results[0]?.output[0]?.result;
         if (!insight) {
           return res.customError({
-            body: 'Execute agent failed with empty response!',
+            body: i18n.translate('assistant.server.error.workspaceDataSourceNotFound', {
+              defaultMessage: 'Workspace/data source is invalid or not found!!',
+            }),
             statusCode: 500,
           });
         }
