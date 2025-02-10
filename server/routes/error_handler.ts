5e13e0e95c5cb72193b62689b6feef9cca5ef2db
hutiechuan <hutiech@amazon.com>
2025-02-10 14:38:53 +0800

    revert yarn.lock and change error_handler
    Signed-off-by: hutiechuan <hutiech@amazon.com>

--- server/routes/error_handler.ts
+++ server/routes/error_handler.ts
@@ -31,6 +31,11 @@ export const handleError = (e: any, res: OpenSearchDashboardsResponseFactory, lo
   // handle http response error of calling backend API
   if (e.statusCode) {
     if (e.statusCode >= 400 && e.statusCode <= 499) {
+      let message = typeof e.body === 'string' ? e.body : JSON.stringify(e.body);
+      if (!message) {
+        message = e.message;
+      }
+
       return res.customError({
         body: { message: typeof e.body === 'string' ? e.body : JSON.stringify(e.body) },
         statusCode: e.statusCode,
--- server/utils/get_opensearch_client_transport.ts
+++ server/utils/get_opensearch_client_transport.ts
@@ -5,10 +5,16 @@
 
 import { OpenSearchClient, RequestHandlerContext } from '../../../../src/core/server';
 
+interface ErrorWithStatusCode extends Error {
+  statusCode?: number;
+}
+
 export class DataSourceNotFoundError extends Error {
-  constructor(message: string) {
+  public readonly statusCode: number;
+  constructor(message: string, originalError?: ErrorWithStatusCode) {
     super(message);
     this.name = 'DataSourceNotFoundError';
+    this.statusCode = originalError?.statusCode || 403;
   }
 }
 
@@ -29,7 +35,7 @@ export const getOpenSearchClientTransport = async ({
     try {
       return (await context.dataSource.opensearch.getClient(dataSourceId)).transport;
     } catch (err) {
-      throw new DataSourceNotFoundError('Saved object does not belong to the workspace');
+      throw new DataSourceNotFoundError('Saved object does not belong to the workspace', err);
     }
   }
   return context.core.opensearch.client.asCurrentUser.transport;
