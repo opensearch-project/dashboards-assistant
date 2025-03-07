4165f4aab253ff949c89235be3efc70457277015
Yulong Ruan <ruanyl@amazon.com>
2025-03-07 14:13:05 +0800

    show more details error msg
    clear previous visualization when encounter error
    
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>

--- public/utils/pipeline/ppl_sample_task.ts
+++ public/utils/pipeline/ppl_sample_task.ts
@@ -24,15 +24,22 @@ export class PPLSampleTask extends Task<Input, Input & { sample: string }> {
 
   async execute<T extends Input>(v: T) {
     const ppl = topN(v.ppl, v.pplSampleSize);
-    const res = await this.searchClient
-      .search(
-        { params: { body: { query: ppl } }, dataSourceId: v.dataSourceId },
-        { strategy: 'pplraw' }
-      )
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      .toPromise<any>();
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    let res: any;
+    try {
+      res = await this.searchClient
+        .search(
+          { params: { body: { query: ppl } }, dataSourceId: v.dataSourceId },
+          { strategy: 'pplraw' }
+        )
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        .toPromise<any>();
+    } catch (e) {
+      throw new Error(`Failed to execute the generated query: '${v.ppl}'.`);
+    }
+
     if (res.rawResponse.total === 0) {
-      throw new Error(`There is no result with the generated query: '${v.ppl}'.`);
+      throw new Error(`There were no results from the query: '${v.ppl}'.`);
     }
     return { ...v, sample: res.rawResponse };
   }
