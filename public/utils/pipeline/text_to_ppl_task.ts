bdea00d9217af89d364c9df8c5cbba775113e997
Lin Wang <wonglam@amazon.com>
2025-03-12 16:19:32 +0800

    Add http error instruction for t2ppl task (#502)
    * Add http error instruction for t2ppl task
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    * Add changelog
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    * Fix typo
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    ---------
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>

--- public/utils/pipeline/text_to_ppl_task.ts
+++ public/utils/pipeline/text_to_ppl_task.ts
@@ -7,6 +7,14 @@ import { HttpSetup } from '../../../../../src/core/public';
 import { Task } from './task';
 import { TEXT2VIZ_API } from '../../../common/constants/llm';
 
+export const processInputQuestion = (inputQuestion: string) => {
+  const httpErrorInstructionAppendRegExp = new RegExp(`(?:${['error', 'fail'].join('|')})`, 'i');
+  if (httpErrorInstructionAppendRegExp.test(inputQuestion)) {
+    return `${inputQuestion}. If you're dealing logs with http response code, then error usually refers to http response code like 4xx, 5xx`;
+  }
+  return inputQuestion;
+};
+
 interface Input {
   inputQuestion: string;
   index: string;
@@ -24,7 +32,7 @@ export class Text2PPLTask extends Task<Input, Input & { ppl: string }> {
   async execute<T extends Input>(v: T) {
     let ppl = '';
     try {
-      ppl = await this.text2ppl(v.inputQuestion, v.index, v.dataSourceId);
+      ppl = await this.text2ppl(processInputQuestion(v.inputQuestion), v.index, v.dataSourceId);
     } catch (e) {
       throw new Error(
         `Error while generating PPL query with input: ${v.inputQuestion}. Please try rephrasing your question.`
