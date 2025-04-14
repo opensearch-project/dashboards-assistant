2dedf1f04913a6a5404c156d8cebf19c3c990845
SuZhou-Joe <suzhou@amazon.com>
2025-04-14 13:15:32 +0800

    feat: address comment
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>

--- public/components/markdown_with_blink_cursor.test.tsx
+++ public/components/markdown_with_blink_cursor.test.tsx
@@ -5,7 +5,11 @@
 
 import React, { useState } from 'react';
 import { fireEvent, render } from '@testing-library/react';
-import { MarkdownWithBlinkCursor } from './markdown_with_blink_cursor';
+import {
+  MarkdownWithBlinkCursor,
+  getMostRightLeafNode,
+  getNonVoidParent,
+} from './markdown_with_blink_cursor';
 
 const NewMarkdownCursor = () => {
   const [loading, setLoading] = useState<boolean>(true);
@@ -18,6 +22,94 @@ const NewMarkdownCursor = () => {
     </>
   );
 };
+
+describe('getMostRightLeafNode', () => {
+  it('should return right most leaf node', () => {
+    const div = document.createElement('div');
+    const result = getMostRightLeafNode(div);
+    expect(result).toEqual(div);
+  });
+
+  it('should return paragraph node', () => {
+    const div = document.createElement('div');
+    div.innerHTML = `
+      <div>
+        <p>paragraph 1</p>
+        <ul>
+          <li>list item 1</li>
+          <li>list item 2</li>
+        </ul>
+        <p>para graph 2</p>
+      </div>
+    `;
+    const result = getMostRightLeafNode(div) as Element;
+    expect(div.childNodes[2]).toEqual(result);
+  });
+
+  it('should return br node for list item', () => {
+    const div = document.createElement('div');
+    div.innerHTML = `
+      <div>
+        <p>paragraph 1</p>
+        <ul>
+          <li>list item 1</li>
+          <li>list item 2</li>`;
+    const result = getMostRightLeafNode(div) as Element;
+    expect(result.textContent).toEqual('list item 2');
+  });
+
+  it('should return br node when there is an empty line', () => {
+    const div = document.createElement('div');
+    div.innerHTML = `
+      <div>
+        <p>paragraph 1</p>
+        <ul>
+          <li>list item 1</li>
+          <li>list item 2</li>
+    `;
+    const result = getMostRightLeafNode(div) as Element;
+    expect(result.textContent).toMatchInlineSnapshot(`
+      "
+          "
+    `);
+  });
+
+  it('should return itself if no children can be found after ignore check', () => {
+    const div = document.createElement('div');
+    div.innerHTML = `
+    
+    
+    
+    `;
+    const result = getMostRightLeafNode(div, (node) => !node.textContent?.trim());
+    expect(result).toEqual(div);
+  });
+});
+
+describe('getNonVoidParent', () => {
+  it('should return parent', () => {
+    const div = document.createElement('div');
+    div.innerHTML = `
+      <div>
+        <p>paragraph 1</p>
+        <ul>
+          <li>list item 1</li>
+          <li>list item 2</li>
+        </ul>
+        <p>para graph 2</p>
+        <img />
+      </div>
+    `;
+
+    const result = getNonVoidParent(div.querySelector('img'));
+
+    expect(result).toEqual(div.childNodes[1]);
+  });
+
+  it('should return null if input is null', () => {
+    expect(getNonVoidParent(null)).toEqual(null);
+  });
+});
 
 describe('<MarkdownWithBlinkCursor />', () => {
   it('should not append assistant_blinkCursor when not loading', () => {
@@ -32,5 +124,14 @@ describe('<MarkdownWithBlinkCursor />', () => {
     expect(container.querySelector('.assistant_blinkCursor')).toBeInTheDocument();
     await fireEvent.click(getByTestId('btn'));
     expect(container.querySelector('.assistant_blinkCursor')).toBeNull();
+  });
+
+  it('should append blink cursor element into correct position', async () => {
+    const markdown = `\n- list item 1\n- list item 2`;
+    const { container, findByTestId, getByTestId } = render(
+      <MarkdownWithBlinkCursor loading>{markdown}</MarkdownWithBlinkCursor>
+    );
+    await findByTestId('AssistantBlinkCursor');
+    expect(getByTestId('AssistantBlinkCursor').parentNode?.textContent).toEqual('list item 2');
   });
 });
--- public/components/markdown_with_blink_cursor.tsx
+++ public/components/markdown_with_blink_cursor.tsx
@@ -4,23 +4,93 @@
  */
 
 import { EuiMarkdownFormat, EuiMarkdownFormatProps } from '@elastic/eui';
-import React from 'react';
-import { useRef } from 'react';
-import { useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 
-function getLastTextNode(node: HTMLElement | Node): Node | null | undefined {
-  if (node.nodeType === node.TEXT_NODE) {
+const voidElements = [
+  'area',
+  'base',
+  'br',
+  'col',
+  'embed',
+  'hr',
+  'img',
+  'input',
+  'link',
+  'meta',
+  'param',
+  'source',
+  'track',
+  'wbr',
+];
+
+function isVoidElements(tagName: string | null) {
+  if (!tagName) {
+    return true;
+  }
+
+  return voidElements.includes(tagName.toLowerCase());
+}
+
+function getTagName(node: Node | HTMLElement): string | null {
+  // Check if the node is an element node
+  if (node.nodeType === Node.ELEMENT_NODE) {
+    // It's safe to cast to Element and access tagName
+    return (node as Element).tagName.toLowerCase();
+  }
+
+  // For non-element nodes (text nodes, comment nodes, etc.)
+  return null;
+}
+
+/**
+ * Find most right leaf node
+ * @param node
+ * @returns
+ */
+export function getMostRightLeafNode(
+  node: HTMLElement | Node,
+  ifIgnore: (node: Node) => boolean = () => false
+): Node {
+  if (node.childNodes.length === 0) {
     return node;
   }
-  const children = node.childNodes;
-  for (let i = children.length - 1; i >= 0; i--) {
-    const child = children[i];
-    const result = getLastTextNode(child);
-    if (result) {
-      return result;
+
+  for (let i = node.childNodes.length - 1; i >= 0; i--) {
+    const child = node.childNodes[i];
+    if (ifIgnore(child)) {
+      continue;
     }
+
+    return getMostRightLeafNode(child, ifIgnore);
+  }
+
+  return node;
+}
+
+/**
+ * Find last non-void node so that the blink cursor can be append into that node
+ * @param node
+ * @returns
+ */
+export function getNonVoidParent(node: ParentNode | Node | null): Node | null | undefined {
+  if (!node) {
     return null;
   }
+
+  if (isVoidElements(getTagName(node))) {
+    return getNonVoidParent(node.parentNode);
+  }
+
+  return node;
+}
+
+/**
+ * Ignore node that will be empty after trim
+ * @param node
+ * @returns
+ */
+export function ignoreEmptyNode(node: Node): boolean {
+  return !node.textContent?.trim();
 }
 
 export const MarkdownWithBlinkCursor = (props: EuiMarkdownFormatProps & { loading?: boolean }) => {
@@ -28,11 +98,13 @@ export const MarkdownWithBlinkCursor = (props: EuiMarkdownFormatProps & { loadin
   useEffect(() => {
     let blinkCursorNode: undefined | HTMLSpanElement;
     if (props.loading && ref.current) {
-      const lastNode = getLastTextNode(ref.current);
+      const mostRightLeafNode = getMostRightLeafNode(ref.current, ignoreEmptyNode);
+      const lastNonVoidNode = getNonVoidParent(mostRightLeafNode);
       blinkCursorNode = document.createElement('span');
       blinkCursorNode.classList.add('assistant_blinkCursor');
-      if (lastNode) {
-        lastNode.parentNode?.appendChild(blinkCursorNode);
+      blinkCursorNode.dataset.testSubj = 'AssistantBlinkCursor';
+      if (lastNonVoidNode) {
+        lastNonVoidNode.appendChild(blinkCursorNode);
       }
     }
 
