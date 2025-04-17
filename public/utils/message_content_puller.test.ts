8249d555c98aaa7f5dc6c4fda6c8c126358b20f6
SuZhou-Joe <suzhou@amazon.com>
2025-04-17 14:36:17 +0800

    feat: refactor message_content_puller to accept isContentReadyToUse param
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>

--- public/utils/message_content_puller.test.ts
+++ public/utils/message_content_puller.test.ts
@@ -21,21 +21,64 @@ describe('MessageContentPool', () => {
     messageContentPool.start();
     messageContentPool.inputComplete();
 
-    await waitFor(() => {
-      expect(subscriptionMock).toHaveBeenCalledTimes(3);
-      expect(subscriptionMock).toHaveBeenNthCalledWith(1, {
-        messageContent: 'a'.repeat(10),
-        messageId: 'a',
-      });
-      expect(subscriptionMock).toHaveBeenNthCalledWith(2, {
-        messageContent: 'b'.repeat(10),
-        messageId: 'b',
-      });
-      expect(subscriptionMock).toHaveBeenNthCalledWith(3, {
-        messageContent: 'a'.repeat(10),
-        messageId: 'a',
-      });
-      expect(completeSubscriptionMock).toHaveBeenCalledTimes(1);
+    await waitFor(
+      () => {
+        expect(subscriptionMock).toHaveBeenCalledTimes(3);
+        expect(subscriptionMock).toHaveBeenNthCalledWith(1, {
+          messageContent: 'a'.repeat(10),
+          messageId: 'a',
+        });
+        expect(subscriptionMock).toHaveBeenNthCalledWith(2, {
+          messageContent: 'b'.repeat(10),
+          messageId: 'b',
+        });
+        expect(subscriptionMock).toHaveBeenNthCalledWith(3, {
+          messageContent: 'a'.repeat(10),
+          messageId: 'a',
+        });
+        expect(completeSubscriptionMock).toHaveBeenCalledTimes(1);
+      },
+      {
+        timeout: 10000,
+      }
+    );
+  });
+
+  it('should buffer content when isContentReadyToUse returns false', async () => {
+    const messageContentPool = new MessageContentPuller({
+      isContentReadyToUse: (message: string) => {
+        if (message.endsWith(')')) {
+          return true;
+        }
+
+        return false;
+      },
     });
+    const subscriptionMock = jest.fn();
+    const completeSubscriptionMock = jest.fn();
+    messageContentPool.addMessageContent('a', `a[hyberlink`);
+    messageContentPool.addMessageContent('a', `](href`);
+    messageContentPool.addMessageContent('a', `)`);
+    const output$ = messageContentPool.getOutput$();
+    output$.subscribe({
+      next: subscriptionMock,
+      complete: completeSubscriptionMock,
+    });
+    messageContentPool.start();
+    messageContentPool.inputComplete();
+
+    await waitFor(
+      () => {
+        expect(subscriptionMock).toHaveBeenCalledTimes(1);
+        expect(subscriptionMock).toHaveBeenNthCalledWith(1, {
+          messageContent: 'a[hyberlink](href)',
+          messageId: 'a',
+        });
+        expect(completeSubscriptionMock).toHaveBeenCalledTimes(1);
+      },
+      {
+        timeout: 10000,
+      }
+    );
   });
 });
--- public/utils/message_content_puller.ts
+++ public/utils/message_content_puller.ts
@@ -15,6 +15,8 @@ const DEFAULT_CONTENT_SLICE_LENGTH = 10;
 interface IMessageContentPullerOptions {
   jobInterval: number;
   contentSliceLength: number;
+  maxBufferLength: number;
+  isContentReadyToUse: (messageContent: string) => boolean;
 }
 
 export class MessageContentPuller {
@@ -25,51 +27,83 @@ export class MessageContentPuller {
   private outputSubscriber: Subscriber<{ messageId: string; messageContent: string }> | null = null;
   private output$: Observable<{ messageId: string; messageContent: string }>;
   private inputCompleted: boolean = false;
+  /**
+   * This map is used to cache last slice length in case sliced content can not be flushed.
+   */
+  private lastContentSliceLengthMap: Record<string, number> = {};
   private options: IMessageContentPullerOptions;
-  constructor(options?: IMessageContentPullerOptions) {
+  constructor(options?: Partial<IMessageContentPullerOptions>) {
     const {
       jobInterval = DEFAULT_JOB_INTERVAL,
       contentSliceLength = DEFAULT_CONTENT_SLICE_LENGTH,
+      /**
+       * use contentSliceLength * 5 as default
+       * since hyberlink usually occupy 20-30 length
+       */
+      maxBufferLength = contentSliceLength * 5,
+      isContentReadyToUse = () => true,
     } = options || {};
     this.messageContentChunk$ = new BehaviorSubject<Record<string, string>>({});
     this.options = {
       jobInterval,
       contentSliceLength,
+      maxBufferLength,
+      isContentReadyToUse,
     };
     this.output$ = new Observable((subscriber) => {
       this.outputSubscriber = subscriber;
     });
   }
 
+  private handleSingleMessage(messageId: string) {
+    const messageContent = this.messageContentChunk$.getValue()[messageId];
+    const contentSliceLength =
+      (this.lastContentSliceLengthMap[messageId] || 0) + this.options.contentSliceLength;
+    const messageContentCandidate = messageContent.slice(0, contentSliceLength);
+    const restContent = messageContent.slice(contentSliceLength);
+
+    /**
+     * We will emit the content when:
+     *
+     * 1. Input stream is closed and all the content has been pulled.
+     * 2. The slice length exceeds the max buffer length client defined.
+     * 3. The content is good to emit without special characters like hyberlink.
+     */
+    if (
+      (this.inputCompleted && !restContent.length) ||
+      contentSliceLength >= this.options.maxBufferLength ||
+      this.options.isContentReadyToUse(messageContentCandidate)
+    ) {
+      this.outputSubscriber?.next({
+        messageId,
+        messageContent: messageContentCandidate,
+      });
+
+      const currentChunkMap = { ...this.messageContentChunk$.getValue() };
+      if (restContent.length > 0) {
+        currentChunkMap[messageId] = restContent;
+      } else {
+        delete currentChunkMap[messageId];
+      }
+
+      this.messageContentChunk$.next(currentChunkMap);
+      this.lastContentSliceLengthMap[messageId] = 0;
+    } else {
+      this.lastContentSliceLengthMap[messageId] = contentSliceLength;
+    }
+  }
+
   private startMessageContentJob() {
     this.messageContentTimer = setTimeout(() => {
       try {
-        const restContents = Object.entries(this.messageContentChunk$.getValue()).reduce(
-          (acc, cur) => {
-            const [messageId, messageContent] = cur;
-            this.outputSubscriber?.next({
-              messageId,
-              messageContent: messageContent.slice(0, this.options.contentSliceLength),
-            });
-            const restContent = messageContent.slice(this.options.contentSliceLength);
-            if (restContent.length > 0) {
-              return {
-                ...acc,
-                [messageId]: restContent,
-              };
-            }
-            return acc;
-          },
-          {}
-        );
+        Object.keys(this.messageContentChunk$.getValue()).forEach((messageId) => {
+          this.handleSingleMessage(messageId);
+        });
 
-        // Update the content pool with remaining content
-        this.messageContentChunk$.next(restContents);
-
-        if (this.inputCompleted && !Object.keys(restContents).length) {
+        if (this.inputCompleted && !Object.keys(this.messageContentChunk$.getValue()).length) {
           this.stop();
         } else {
-          // Schedule the next processing cycle
+          // Schedule next processing cycle
           this.startMessageContentJob();
         }
       } catch (e) {
@@ -111,6 +145,8 @@ export class MessageContentPuller {
       this.messageContentChunk$.complete();
       this.messageContentChunk$.unsubscribe();
     }
+
+    this.lastContentSliceLengthMap = {};
   }
 
   /**
